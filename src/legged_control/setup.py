c5782d39631e2d0617ca7bd7f1410d833b07e0b5
firommao <grayred223@gmail.com>
2026-04-08 13:50:32 +0800

    feat(motor-bench): register motor_bench_node entry point and launch file

--- src/legged_control/setup.py
+++ src/legged_control/setup.py
@@ -10,16 +10,20 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/config', ['config/robot.yaml']),
-        ('share/' + package_name + '/launch', ['launch/locomotion.launch.py']),
+        ('share/' + package_name + '/launch', [
+            'launch/locomotion.launch.py',
+            'launch/motor_bench.launch.py',
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     entry_points={
         'console_scripts': [
-            'joint_aggregator = legged_control.joint_aggregator:main',
-            'teleop_node      = legged_control.teleop_node:main',
-            'policy_node      = legged_control.policy_node:main',
-            'watchdog_node    = legged_control.watchdog_node:main',
+            'joint_aggregator  = legged_control.joint_aggregator:main',
+            'teleop_node       = legged_control.teleop_node:main',
+            'policy_node       = legged_control.policy_node:main',
+            'watchdog_node     = legged_control.watchdog_node:main',
+            'motor_bench_node  = legged_control.motor_bench_node:main',
         ],
     },
 )
