9d36b5a34ea6fa7089654768fbe77dbcf625b874
FiromMao <grayred223@gmail.com>
2026-04-08 16:01:12 +0800

    chore: scaffold legged_control package

---
+++ src/legged_control/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+package_name = 'legged_control'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['tests*']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/config', ['config/robot.yaml']),
+        ('share/' + package_name + '/launch', ['launch/robot.launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [],
+    },
+)
